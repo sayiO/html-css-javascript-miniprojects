0b7a71ad9e19e3a75970f0eeafff6cdd1ae8c76f
sayiO <sayionias@gmail.com>
2021-06-14 18:02:30 +0200

    styled the scroll top button using javascript

--- main.js
+++ main.js
@@ -4,9 +4,22 @@ window.addEventListener("scroll", scrollFunction);
 
 function scrollFunction() {
     if (window.pageYOffset > 300) {
-        backToTopButton.style.display = "block";
-    } else {
-        backToTopButton.style.display = none;
+        if (!backToTopButton.classList.contains("btnEntrance")) {
+            backToTopButton.classList.remove("btnExit");
+            backToTopButton.classList.add("btnEntrance")
+            backToTopButton.style.display = "block";
+        } else {
+            if (backToTopButton.classList.contains("btnEntrance")) {
+                backToTopButton.classList.remove("btnEntrance");
+                backToTopButton.classList.add("btnExit");
+                setTimeout(function() {
+                    backToTopButton.style.display = none;
+
+
+                }, 250);
+
+            }
+        }
     }
 }
 
